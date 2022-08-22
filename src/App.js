1dddd249492cc0b49a56be71eefcd8fba2686670
jang <dkanvk1@gmail.com>
2022-08-22 14:45:55 +0900

    finish

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Movies from "./routes/Movies";
+import movie from "./routes/Movies";
+function App() {
+  return <div>
+    <Movies></Movies>
+  </div>;
+}
+
+export default App;
